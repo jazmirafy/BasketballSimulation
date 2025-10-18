a53691bdefd217a8360e37b602016a9b63762639
jazmirafy <egyptjazmean@gmail.com>
2025-10-18 16:26:13 -0500

    Added script to sync nDisplay transform and player camera transform
    now n display transform updates with the player. current bug: the nDisplay can collide with the player which limits player mobility and causes the player to shoot up to the sky when the player looks down and nDisplay config collides with the floor

---
+++ Source/BasketballSimulation/NDisplayCameraSyncer.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "Components/ActorComponent.h"
+#include "NDisplayCameraSyncer.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BASKETBALLSIMULATION_API UNDisplayCameraSyncer : public UActorComponent
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	UNDisplayCameraSyncer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UCameraComponent* PlayerCameraComponent;
+	void SyncCameras(); //handles copying the player camera position to the nDisplay position so nDisplay is synced with the player first person view
+
+};
