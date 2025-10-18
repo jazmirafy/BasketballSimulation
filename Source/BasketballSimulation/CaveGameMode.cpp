ea347229a1d732fd48eef422a1ee1ed5a76cb210
jazmirafy <egyptjazmean@gmail.com>
2025-10-18 04:47:43 -0500

    nDisplay Now follows the view of the player but is limited and does not yet follow the player rotation

--- Source/BasketballSimulation/CaveGameMode.cpp
+++ Source/BasketballSimulation/CaveGameMode.cpp
@@ -1,5 +1,73 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "CaveGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerController.h"
+#include "Camera/CameraComponent.h"
+#include "Camera/CameraActor.h"
+#include "EngineUtils.h"
 
+
+ACaveGameMode::ACaveGameMode()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	if (PlayerPawnBPClass.Succeeded())
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonPlayerController"));
+	if (PlayerControllerBPClass.Succeeded())
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
+
+void ACaveGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Error, TEXT("Log is working"));
+
+	//assign the ndisplay and player to our variables
+	PlayerCameraActor = GetWorld()->GetFirstPlayerController()->GetViewTarget();
+	for (TActorIterator<AActor> It(GetWorld()); It; ++It)
+	{
+		if (It->ActorHasTag("nDisplay Anchor")) 
+		{
+			NDisplayCameraActor = *It;
+			break;
+		}
+	}
+	//see if we found the ndisplay and the player
+	if (PlayerCameraActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player found: %s"), *PlayerCameraActor->GetName());
+	
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("No player found!"));
+	}
+	if (NDisplayCameraActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("nDisplay found: %s"), *NDisplayCameraActor->GetName());
+
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("No nDisplay found!"));
+	}
+}
+void ACaveGameMode::Tick(float DeltaSeconds) {
+	Super::Tick(DeltaSeconds);
+	SyncCameras(); //run sync cameras each frame so the cave nDisplay accurately represents the player first person view
+
+}
+void ACaveGameMode::SyncCameras() {
+	//if references to both the player camera and nDisplay are valid, assign the players camera transform to nDisplay
+	if (PlayerCameraActor && NDisplayCameraActor) {
+		FTransform PlayerTransform = PlayerCameraActor->GetActorTransform();
+		NDisplayCameraActor->SetActorTransform(PlayerTransform);
+	}
+}
--- Source/BasketballSimulation/CaveGameMode.h
+++ Source/BasketballSimulation/CaveGameMode.h
@@ -13,5 +13,20 @@ UCLASS()
 class BASKETBALLSIMULATION_API ACaveGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+
+	ACaveGameMode();
+
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+
+	AActor* PlayerCameraActor;
+	AActor* NDisplayCameraActor;
+	void SyncCameras(); //handles copying the player camera position to the nDisplay position so nDisplay is synced with the player first person view
 	
 };
