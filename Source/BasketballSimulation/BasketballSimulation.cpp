85ad236ce6f9b2bc3328bf58ef6ef9abdcd0f228
jazmirafy <egyptjazmean@gmail.com>
2025-10-19 17:06:12 -0500

    Deleted the 5.4.4 project and migrated the new 5.2 project to the repository

--- Source/BasketballSimulation/BasketballSimulation.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BasketballSimulation.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BasketballSimulation, "BasketballSimulation" );
